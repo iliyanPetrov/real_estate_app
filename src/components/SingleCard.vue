0759a5bcca8b2e87e872df23119594ae23d53b11
iliyanPetrov <iliyan.petrov1986@gmail.com>
2024-07-31 03:18:01 +0300

    second commit _ some style test

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -7,12 +7,28 @@ defineProps( {
 <template>
     <article>
         <header> {{ house.cityName }} </header>
+        <!-- make header clickable to open other houses in the same city -->
         <div class="wrapper">
             <section>
                 some house picture
             </section>
-            <p> rooms: {{ house.roomsCount }} </p>
-            <p> price: {{ house.price }} </p>
+            <aside>
+                <span class="title"> neighborhood:</span>
+                <span> {{ house.neighborhood }} </span>
+                <span class="title"> owner:</span>
+                <span>{{ house.ownerName }} </span>
+                <span class="title"> phone:</span>
+                <span> {{ house.ownerPhone }} </span>
+                <span class="title"> email: </span>
+                <span>{{ house.ownerEmail }} </span>
+                <!-- make this  <p> clickable -->
+                <span class="bottom"> more details...</span>
+            </aside>
+            <footer>
+                <span> $ {{ house.price }} </span>
+                <span> rooms: {{ house.roomsCount }} </span>
+            </footer>
+            <button> Compare </button>
         </div>
     </article>
 </template>
@@ -25,15 +41,71 @@ article {
     border-radius: 5px;
 }
 
+header {
+    text-align: center;
+    font-size: 1.5rem;
+    font-weight: 700;
+    line-height: 3rem;
+    color: var(--secondary);
+}
+
 .wrapper {
     display: grid;
-    grid-template-columns: 2fr 1fr;
-    grid-template-rows: 5fr 1fr; /* fix that */    
-    gap: 1rem;
+    grid-template-columns: 3fr 1fr;
+    grid-template-rows: 6fr 1fr;
+    /* fix that */
+    gap: 0.5rem;
 
     section {
         min-height: 240px;
+    }
+
+    section {
+        background-image: url(../assets/house-1.png);
+    }
+
+    section,
+    aside,
+    footer {
         border: 2px solid var(--secondary);
     }
+}
+
+aside {
+    min-height: 300px;
+    max-width: 150px;
+    display: flex;
+    flex-direction: column;
+    gap: 0.25rem;
+    padding: 0.5rem;
+
+    .title {
+        font-weight: 700;
+        margin-top: 8px;
+    }
+
+    .bottom {
+        text-align: center;
+        margin-top: auto;
+        margin-bottom: 8px;
+        border: 3px double var(--secondary);
+        border-radius: 5rem;
+        cursor: pointer;
+        /* flex: 1; */
+        /* justify-self: flex-end; */
+        /* why isn't this working? */
+    }
+    .bottom:hover {
+        transition: 0.35s;
+        background-color: rgb(237, 217, 247);
+        border-color: var(--gray-soft);
+    }
+}
+
+footer {
+    padding: 0 4px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 </style>
